cce479ce0057c6e97797e4f27956eebc014ab7cc
VinutaRMadival <88669384+VinutaRMadival@users.noreply.github.com>
2021-09-02 19:52:48 +0530

    Delete src/javaday3 directory

--- src/javaday3/Abstract1.java
+++
@@ -1,8 +0,0 @@
-package javaday3;
-
-public abstract class Abstract1
-{
-	
-abstract void run();     //abstract method
-	
-}
--- src/javaday3/Shape.java
+++
@@ -1,6 +0,0 @@
-package javaday3;
-
-public abstract class Shape {
-	public abstract void draw();//abstract method
-    public abstract void print();//abstract method
-}
